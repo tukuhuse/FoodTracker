6330936e620fc09fe46e51ca0974413e9dc57fca
tukuhuse <tukuhuse@icloud.com>
2020-12-30 18:33:25 +0900

    Initial Commit

---
+++ FoodTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodTracker
+//
+//  Created by 高橋優人 on 2020/12/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodTracker/FoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackerApp.swift
+//  FoodTracker
+//
+//  Created by 高橋優人 on 2020/12/30.
+//
+
+import SwiftUI
+
+@main
+struct FoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
