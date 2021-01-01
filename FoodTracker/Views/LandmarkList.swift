997f58a1586235ae6491d62fcc22368c7dfacf60
tukuhuse <tukuhuse@icloud.com>
2021-01-01 23:32:39 +0900

    section4完了

---
+++ FoodTracker/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  FoodTracker
+//
+//  Created by 高橋優人 on 2021/01/01.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
